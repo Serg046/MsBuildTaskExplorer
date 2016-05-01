fcb2aa230fefe747420100b72e58e3e0899de548
Serg046 <serg046@outlook.com>
2016-05-01 03:32:46 +0300

    Issue #7: Application settings

---
+++ MsBuildTaskExplorer/Settings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsBuildTaskExplorer
+{
+    public class Settings
+    {
+        private static class SettingNames
+        {
+            public const string FILTER = "Filter";
+            public const string EXPANDED_TARGETS = "ExpandedTargets";
+        }
+
+        public static Settings Instance { get; } = new Settings();
+
+        public string Filter
+        {
+            get
+            {
+                return ReadSetting(SettingNames.FILTER); 
+            }
+            set
+            {
+                SaveSetting(SettingNames.FILTER, value);
+            }
+        }
+
+        public string ExpandedTargets
+        {
+            get
+            {
+                return ReadSetting(SettingNames.EXPANDED_TARGETS);
+            }
+            set
+            {
+                SaveSetting(SettingNames.EXPANDED_TARGETS, value);
+            }
+        }
+
+        private string ReadSetting(string settingName) => ConfigurationSettings.AppSettings[settingName];
+
+        private void SaveSetting(string settingName, string value)
+            => ConfigurationSettings.AppSettings[settingName] = value;
+    }
+}
--- MsBuildTaskExplorer/TaskExplorerWindowControl.xaml.cs
+++ MsBuildTaskExplorer/TaskExplorerWindowControl.xaml.cs
@@ -1,22 +1,10 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="TaskExplorerWindowControl.xaml.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Security;
-using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
-using Microsoft.Build.Logging;
-using Microsoft.Build.Utilities;
 
 namespace MsBuildTaskExplorer
 {
@@ -26,17 +14,22 @@ namespace MsBuildTaskExplorer
     {
         private readonly SolutionInfo _solutionInfo = new SolutionInfo();
         private bool _isInitialized;
+        private const string SEPARATOR = "<`~`>";
 
         public TaskExplorerWindowControl()
         {
             this.InitializeComponent();
             Loaded += OnLoaded;
+            Unloaded += (sender, args) => SaveSettings();
         }
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
             if (!_isInitialized)
             {
+                var filter = Settings.Instance.Filter;
+                if (!string.IsNullOrEmpty(filter))
+                    FilterTb.Text = filter;
                 _solutionInfo.Initialize();
                 if (_solutionInfo.IsOpen)
                 {
@@ -65,6 +58,30 @@ namespace MsBuildTaskExplorer
                 })
                 .Where(t => t.Targets != null && t.Targets.Any())
                 .OrderBy(t => t.FilePath);
+
+            if (!string.IsNullOrEmpty(FilterTb.Text))
+            {
+                foreach (var item in TasksItemsControl.Items)
+                {
+                    var dObject = TasksItemsControl.ItemContainerGenerator.ContainerFromItem(item);
+                    ((TreeViewItem)dObject).IsExpanded = true;
+                }
+            }
+            else
+            {
+                var expandedTargets = Settings.Instance.ExpandedTargets;
+                if (!string.IsNullOrEmpty(expandedTargets))
+                {
+                    var targetPaths = expandedTargets.Split(new[] { SEPARATOR }, StringSplitOptions.None);
+                    var itemsToExpand = TasksItemsControl.Items.Cast<object>()
+                        .Select(item => ((TreeViewItem)TasksItemsControl.ItemContainerGenerator.ContainerFromItem(item)))
+                        .Where(item => targetPaths.Contains(((MsBuildTask)item.DataContext).FilePath));
+                    foreach (var treeViewItem in itemsToExpand)
+                    {
+                        treeViewItem.IsExpanded = true;
+                    }
+                }
+            }
         }
 
         private void RunButtonOnClick(object sender, RoutedEventArgs e)
@@ -100,7 +117,23 @@ namespace MsBuildTaskExplorer
 
         private void RefreshButtonOnClick(object sender, RoutedEventArgs e)
         {
+            SaveSettings();
             UpdateTaskList();
+        }
+
+        private void SaveSettings()
+        {
+            if (!string.IsNullOrEmpty(FilterTb.Text))
+                Settings.Instance.Filter = FilterTb.Text;
+            
+            var expandedItems = TasksItemsControl.Items.Cast<object>()
+                .Select(item => ((TreeViewItem)TasksItemsControl.ItemContainerGenerator.ContainerFromItem(item)))
+                .Where(item => item.IsExpanded)
+                .Select(item => ((MsBuildTask)item.DataContext).FilePath)
+                .Where(path => !Regex.IsMatch(path, SEPARATOR))
+                .ToList();
+            if (expandedItems.Any())
+                Settings.Instance.ExpandedTargets = string.Join(SEPARATOR, expandedItems);
         }
     }
 }
